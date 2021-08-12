609ba6d367deb8605a0f4b36144ce5a9f43ca474
Yassine A <64434215+NarutoClover@users.noreply.github.com>
2021-08-11 22:01:33 -0400

    Add files via upload

---
+++ reducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+  //The data is stored as a dictionnary
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    //use cases to trigger a change in the data layer
+    default:
+      return state;
+  }
+};
+
+export default reducer;
